b763e68b1368abbd428438c4d85622901ba447ff
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-04-11 11:46:07 +0200

    aggiustato responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,13 +43,3 @@ var app = new Vue({
 
   },
 })
-
-// METODO VUE PER LA RICHIESTA DI API
-// axios
-// .get('https://flynn.boolean.careers/exercises/api/random/boolean')
-// .then(function (response) {
-//   const result = response.data;
-// })
-// .catch((error) => {
-//   console.log(error);
-// });
